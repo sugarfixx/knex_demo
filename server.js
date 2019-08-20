1da355d431c9a286ef84471a47993cece8b5413c
sugarfixx <ingar.torsrud@me.com>
2019-08-20 09:39:00 +0200

    Adds Dockerfiles to project

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var port = process.env.POST || 8000;
+var port = process.env.POST || 8080;
 var app = express();
 
 app.use(bodyParser.json());
